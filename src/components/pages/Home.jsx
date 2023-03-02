0a3896379b13486e64de038d5ccb2ddfe9758b82
Bkind254 <langatbrian02@gmail.com>
2023-03-02 10:23:27 +0300

    Initial commit

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../App.css";
+import Cards from "../Cards";
+import Footer from "../Footer";
+import Section1 from "../Section1";
+
+function Home() {
+  return (
+    <>
+      <Section1 />
+      <Cards />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
